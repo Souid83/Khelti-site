f6728d77f843a998bfbe4413cb254ccab870299a
Souid83 <swuidy@hotmail.fr>
2025-10-03 18:01:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/cheveux-nature-site/", // IMPORTANT : Ajout du bon chemin pour GitHub Pages
+  base: "/", // ✅ Pour Netlify : déploiement à la racine
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
